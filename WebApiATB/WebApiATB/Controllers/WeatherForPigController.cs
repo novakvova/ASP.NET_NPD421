67c44143ac4d1749c925cd29167c353ba0962c77
novakvova <novakvova@gmail.com>
2025-11-23 17:43:24 +0200

    Update controller

--- WebApiATB/WebApiATB/Controllers/WeatherForPigController.cs
+++ WebApiATB/WebApiATB/Controllers/WeatherForPigController.cs
@@ -4,16 +4,16 @@ namespace WebApiATB.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForPigController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForPigController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForPigController(ILogger<WeatherForPigController> logger)
         {
             _logger = logger;
         }
